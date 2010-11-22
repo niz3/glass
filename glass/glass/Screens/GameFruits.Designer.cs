054826b21a43c5c3473e236b860fede2ed42ae4c
unknown <nn930428@.gavle.se>
2010-11-22 10:40:42 +0100

    Signed-off-by: unknown <nn930428@.gavle.se>

---
+++ glass/glass/Screens/GameFruits.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nn930428
+ * Date: 2010-11-22
+ * Time: 10:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace glass.Screens
+{
+	partial class GameFruits
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// GameFruits
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "GameFruits";
+			this.Name = "GameFruits";
+		}
+	}
+}
---
+++ glass/glass/Screens/GameFruits.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nn930428
+ * Date: 2010-11-22
+ * Time: 10:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace glass.Screens
+{
+	/// <summary>
+	/// Description of GameFruits.
+	/// </summary>
+	public partial class GameFruits : Form
+	{
+		public GameFruits()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
