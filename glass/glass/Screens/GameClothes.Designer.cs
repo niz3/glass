5612f931f62f8b491c9bdb8aa8fb330621871411
unknown <ds930619@.gavle.se>
2010-11-29 09:09:32 +0100

    Signed-off-by: unknown <ds930619@.gavle.se>

---
+++ glass/glass/Screens/GameClothes.Designer.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ds930619
+ * Date: 2010-11-22
+ * Time: 10:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace glass.Screens
+{
+	partial class GameClothes
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			// 
+			// GameClothes
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "GameClothes";
+			this.Name = "GameClothes";
+		}
+	}
+}
---
+++ glass/glass/Screens/GameClothes.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ds930619
+ * Date: 2010-11-22
+ * Time: 10:40
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace glass.Screens
+{
+	/// <summary>
+	/// Description of GameClothes.
+	/// </summary>
+	public partial class GameClothes : Form
+	{
+		public GameClothes()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
